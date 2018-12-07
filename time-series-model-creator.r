90898f9e3aa1f445fd186bb6e41e2450ebf1c1cc
Daniel Hopp <daniel.hopp1@gmail.com>
2018-12-07 16:59:28 +0100

    File import

--- time-series-model-creator.r
+++ time-series-model-creator.r
@@ -1,1 +1,8 @@
+#Select time series data file, expects a file with 2 columns, 1 for the period label, 1 for the values
+myFile <- file.choose()
+data  <- read.csv(myFile)
 
+#rename columns
+names(data) <- c("period", "value")
+
+#
