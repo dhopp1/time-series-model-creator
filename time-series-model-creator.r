c8a4f1f1969d40a214ea8bc852e0c94488ee4871
Dan Hopp <daniel.hopp1@gmail.com>
2018-12-07 17:11:30 +0100

    converting data to timeseries

--- time-series-model-creator.r
+++ time-series-model-creator.r
@@ -5,4 +5,11 @@ data  <- read.csv(myFile)
 #rename columns
 names(data) <- c("period", "value")
 
-#
+#convert to timeseries datatype
+#year of first observation
+year <- 2015
+#period of first observation
+period <- 11
+#frequency of observations, 52 for weekly, 12 for yearly, etc.
+frequency <- 52 
+series <- ts(data$value, start = c(year, period), frequency = frequency)
